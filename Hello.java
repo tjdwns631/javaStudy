cc56333e1ee3d6bdcca5936dc2a95806cb199847
tjdwns631 <tjdwns631@naver.com>
2020-08-10 11:08:59 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+ public static void main(String[] args) {
+     System.out.println("Hello, welcome to the java world!");
+      System.out.println("nice to meet you!");  
+      int myAge = 28;
+      System.out.println("my Age is " + myAge);	  
+      }
+ } 
+ 
+ 
