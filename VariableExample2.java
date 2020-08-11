63e4a397841f9f4cf88b86494e6ae70cbe239b14
Your Name <you@example.com>
2020-08-11 10:50:36 +0900

    ignore add

---
+++ VariableExample2.java
@@ -0,0 +1,10 @@
+public class VariableExample2  {
+   public static void main(String[] args) {
+    int n1 = 15;
+	int n2 = 25 , n3 = 35;
+   System.out.println(n1 + " + " + n2 + " = " +(n1 + n2));
+   System.out.println(n2 + " + " + n3 + " = " +(n2 + n3));
+   System.out.println(n3 + " + " + n1 + " = " +(n3 + n1)); 
+     }
+  
+  }
---
+++ VariableExample3.java
@@ -0,0 +1,19 @@
+public class VariableExample3  {
+   public static void main(String[] args) {
+      byte b1 = -128;
+	  byte b2 = 127;
+	  
+	  //System.out.println(b2++);
+      //System.out.println(b2++);
+	  //System.out.println(b2++);
+	  
+	  	   System.out.println(b1--); // b1 = b1 - 1;
+		   System.out.println(b1--); // b1 = b1 - 1;
+	 	   System.out.println(b1--); // b1 = b1 - 1;
+		
+	
+	//for(int i = 0; i < 10; i++) {
+		//System.out.println(b2++);
+	//}
+  }
+}
